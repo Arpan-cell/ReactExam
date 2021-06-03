1a073dd554862885092a85c6d2f2cfb54928de38
Arpan Roy <83748493+Arpan-cell@users.noreply.github.com>
2021-06-03 12:54:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ReactExam from "./ReactExam";
+
+function App() {
+  return (
+    <div className="App">
+      <ReactExam />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ReactExam.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+export default function ReactExam() {
+  const color = "#f5655b";
+  const [change, setChange] = useState(color);
+  const [text, setText] = useState("");
+
+  const changeColor = () => {
+    const color1 = "#23773E";
+    setChange(color1);
+    setText("Button Clicked");
+  };
+  return (
+    <>
+      <div className="change">
+        <div className="col-lg-6 col-md-6 col-sm-6 ">
+          <div className="Leftside card" style={{ backgroundColor: change }}>
+            <div id="hello">
+              <h4 className="hello">Hello</h4>
+              <h4>Hello Hello</h4>
+              <h4>Hello Hello Hello</h4>
+              <h4>Hello Hello Hello Hello</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="button">
+        <button onClick={changeColor} id="button1">
+          Press
+        </button>
+      </div>
+      <div className="text1">
+        <p>
+          <b>{text}</b>
+        </p>
+      </div>
+    </>
+  );
+}
